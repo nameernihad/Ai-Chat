5a9e3ed4b56947e0f23bc030770219c561150906
Nameer Nihad PC <nameernihadpc@gmail.com>
2023-12-26 23:45:15 +0530

    starting of ai-chat application

---
+++ chat-partner/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-amber-50  h-screen" >
+      <div className="bg-slate-800 w-96 h-96" ></div> 
+    </div>
+    </>
+  )
+}
+
+export default App
